8e7ba86741b0c547f38121ca8419dee69fd629df
FishingKing <453814685@qq.com>
2017-10-27 20:33:18 +0800

    添加项目文件。

---
+++ Painter/PainterView.cpp
@@ -0,0 +1,104 @@
+
+// PainterView.cpp : CPainterView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "Painter.h"
+#endif
+
+#include "PainterDoc.h"
+#include "PainterView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CPainterView
+
+IMPLEMENT_DYNCREATE(CPainterView, CView)
+
+BEGIN_MESSAGE_MAP(CPainterView, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CPainterView 构造/析构
+
+CPainterView::CPainterView()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CPainterView::~CPainterView()
+{
+}
+
+BOOL CPainterView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CPainterView 绘制
+
+void CPainterView::OnDraw(CDC* /*pDC*/)
+{
+	CPainterDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CPainterView 打印
+
+BOOL CPainterView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CPainterView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CPainterView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CPainterView 诊断
+
+#ifdef _DEBUG
+void CPainterView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CPainterView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CPainterDoc* CPainterView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CPainterDoc)));
+	return (CPainterDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CPainterView 消息处理程序
